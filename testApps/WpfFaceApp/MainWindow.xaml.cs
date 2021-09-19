8f6fbec75be7ad692cdd10f41ff7f19e36bab4e4
Gang Zhang <gzhangx@hotmail.com>
2021-09-18 20:21:15 -0400

    release wpf

--- testApps/WpfFaceApp/MainWindow.xaml.cs
+++ testApps/WpfFaceApp/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,71 @@ namespace WpfFaceApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        bool videoCaptureThreadRunning = false;
+        Thread videoCaptureThread = null;
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            if (videoCaptureThread == null && !videoCaptureThreadRunning)
+            {
+                int started = VedaFacesDotNet.VedaFaceNative.startVideoCapture(0);
+                if (started == 0)
+                {
+                    MessageBox.Show("Video Start failed");
+                    return;
+                }
+                videoCaptureThread = new Thread(videoCaptureRun);
+                videoCaptureThread.Start();
+                btnStart.Content = "Click To Stop";
+            }
+            else
+            {
+                VedaFacesDotNet.VedaFaceNative.stopVideoCapture();
+                videoCaptureThread = null;
+                btnStart.Content = "Click To Start";
+            }
+        }
+
+        void videoCaptureRun()
+        {
+            try
+            {
+                var img = VedaFacesDotNet.VedaFaceNative.createBgrImg();
+                while (videoCaptureThread != null)
+                {
+                    VedaFacesDotNet.VedaFaceNative.captureVideo(img);
+                    var bmp = VedaFacesDotNet.VedaFaces.imgToBmp(img);
+
+                    uiInvoke(() =>
+                    {
+                        imgMain.Source = Convert(bmp);
+                    });
+                }
+            }
+            finally
+            {
+                videoCaptureThreadRunning = false;
+                VedaFacesDotNet.VedaFaceNative.stopVideoCapture();
+            }
+        }
+
+        public BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            src.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+
+        void uiInvoke(Action act)
+        {
+            Dispatcher.Invoke(act);
         }
     }
 }
